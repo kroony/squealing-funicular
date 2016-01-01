168a5fce2a15aa47c4d670c58b4544b54a4ee2ff
= <=>
2016-01-01 11:22:09 +0000

    gitignore

--- site/templates_c/6c12957380aafd3259936d4a012f59caf067c8dc_0.file.home.tpl.php
+++ site/templates_c/6c12957380aafd3259936d4a012f59caf067c8dc_0.file.home.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.30-dev/14, created on 2016-01-01 11:11:29
+/* Smarty version 3.1.30-dev/14, created on 2016-01-01 11:21:07
   from "/var/kroony/site/home.tpl" */
 
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.30-dev/14',
-  'unifunc' => 'content_56865ee1cfc257_90813045',
+  'unifunc' => 'content_56866123d60de2_25054265',
   'file_dependency' => 
   array (
     '6c12957380aafd3259936d4a012f59caf067c8dc' => 
     array (
       0 => '/var/kroony/site/home.tpl',
-      1 => 1451618112,
+      1 => 1451647225,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_56865ee1cfc257_90813045 ($_smarty_tpl) {
+function content_56866123d60de2_25054265 ($_smarty_tpl) {
 ?>
 <p>Generate Level: <a href="addNew.php?level=1">1</a></p>
 <?php }
