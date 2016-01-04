ad813a66faa5553c55ee44158f82cecbb7f8b8be
William Kerr <kroony@trout-slap.com>
2016-01-04 13:25:45 +1100

    DB update and TODOs

--- site/cron/heal.php
+++ site/cron/heal.php
@@ -8,6 +8,9 @@ include_once("hero/heroController.php");
 $heroController = new heroController();
 $res = $heroController->performGlobalHealing(1);
 
+//@TODO flag somewhere the time this was run and display last heal time somewhere on the site. 
+
+
 //commented out so no AUTO resurections
 /*$db = DB::GetConn();
 $db->query("UPDATE `Hero` SET CurrentHP = 1 WHERE CurrentHP <= 0 and CurrentHP > -Con ORDER BY RAND() LIMIT 1");*/
