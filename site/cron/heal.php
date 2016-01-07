5324da086966779fbb2dd600ea2caffb5d328030
William Kerr <kroony@trout-slap.com>
2016-01-07 14:41:37 +1100

    auto resurrect monster hero

--- site/cron/heal.php
+++ site/cron/heal.php
@@ -11,8 +11,8 @@ $res = $heroController->performGlobalHealing(0.1);
 //@TODO flag somewhere the last time this was run and display it somewhere on the site. 
 
 
-//commented out so no AUTO resurections
-/*$db = DB::GetConn();
-$db->query("UPDATE `Hero` SET CurrentHP = 1 WHERE CurrentHP <= 0 and CurrentHP > -Con ORDER BY RAND() LIMIT 1");*/
+//Auto Resurect Monster Heros
+$db = DB::GetConn();
+$db->query("UPDATE `Hero` SET CurrentHP = 1 WHERE CurrentHP <= 0 and CurrentHP > -Con and OwnerID = 146 ORDER BY RAND() LIMIT 1");
 
 ?>
