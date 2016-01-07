b3ae2eb99ae591f33de24244de6de49db0be241e
William Kerr <kroony@trout-slap.com>
2016-01-08 02:04:55 +1100

    DB fix for Classes having Intel as Int
    Already Run the Refactor

---
+++ migrations/00005_refactor.sql
@@ -0,0 +1,6 @@
+UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 19;
+UPDATE `AdvClass` SET `PrerequisiteAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 19;
+UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 26;
+UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 27;
+UPDATE `AdvClass` SET `PrerequisiteAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 27;
+
--- site/index.php
+++ site/index.php
@@ -6,6 +6,14 @@ include_once("hero/race.php");
 
 $db = DB::GetConn();
 
+$getrefactor5Query = "UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 19;
+UPDATE `AdvClass` SET `PrerequisiteAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 19;
+UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 26;
+UPDATE `AdvClass` SET `FavouredAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 27;
+UPDATE `AdvClass` SET `PrerequisiteAttribute` = 'Intel' WHERE `AdvClass`.`ID` = 27;";
+$res=$db->query($getrefactor5Query);//execute query
+echo "refactor";
+
 $getClassQuery = "SELECT Class,COUNT(*) as count FROM Hero Where OwnerID <> 146 GROUP BY Class ORDER BY count DESC;";
 $res=$db->query($getClassQuery);//execute query
 $ClassTableData = "['Class', 'Population'],";
