ba2da1244cf99c8e2ea392e9a006f19c9d379940
William Kerr <kroony@trout-slap.com>
2016-01-07 14:14:22 +1100

    Dead Heros become monsters

--- site/delete.php
+++ site/delete.php
@@ -2,9 +2,22 @@
 
 include_once("bootstrap.php");
 
+include_once("hero/hero.php");
+
+$Hero = new Hero();
+$Hero = $testHero->loadHero($_REQUEST['ID']);
+
+$Hero->GiveToUser(146);//give the Hero to the monster user 
+
+$Hero->Level = -1;
+$Hero->CurrentHP = $Hero->MaxHP;
+
+$Hero->SaveHero();
+
+/*
 $DeleteQuery = "DELETE FROM `kr00ny_sf`.`Hero` WHERE `Hero`.`ID` = " . $_REQUEST["ID"] . ";";
 
 $db = DB::GetConn();
-$db->query($DeleteQuery);
+$db->query($DeleteQuery);*/
 
 header("Location: home.php");
