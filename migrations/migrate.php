2fb4442c5c606be8edeea9c12ff7ffbd51ad36fa
Amos Robinson <amos.robinson@gmail.com>
2016-01-09 21:31:19 +1100

    Playing with migrations script

---
+++ migrations/migrate.php
@@ -0,0 +1,37 @@
+<?php
+
+set_include_path("../site");
+require_once("includes/database.class.php");
+
+$db = DB::GetConn();
+$res = $db->query("SELECT * FROM migrations");
+
+echo "Finding already run migrations\n";
+
+$already_run = array();
+
+while($obj = $res->fetchObject())
+{
+    $already_run[$obj->migration_file] = 1;
+}
+
+echo "Looking for new migrations\n";
+$all = scandir("run");
+
+foreach ($all as $migration) {
+    if (!isset($already_run[$migration])) {
+        echo "Running migration $migration\n";
+        $info = pathinfo($migration);
+        if ($info['extension'] == "php") {
+            include('run/' . $migration);
+        }
+        echo "Inserting migration $migration\n";
+        $insert = array('migration_file' => $migration
+                       , 'time' => time() );
+        $db->insert('migrations', $insert);
+    }
+}
+
+
+
+?>
