c8dc431de9ff358de31d43fc3b1ff41b37d24000
William Kerr <kroony@trout-slap.com>
2016-01-20 18:19:33 +1100

    monster troubleshooting

--- site/login.php
+++ site/login.php
@@ -14,32 +14,32 @@ if(!is_object($obj))
 }
 else
 {
-	if($obj->active == 1)
-	{
-    if($obj->password != "pass")
-    {
-      if(password_verify($_REQUEST['password'], $obj->password))
-      {
-        $_SESSION['userID'] = $obj->ID;
-        $smarty->assign("result","success");
-        header( 'Location: home.php' );
-        exit(0);
-      }
-    }
-    else if($obj->password == $_REQUEST['password'])
-    {
-      //@TODO redirect to change password page, once noone has the password "pass" remove this check
-      $_SESSION['userID'] = $obj->ID;
-      $smarty->assign("result","success");
-      header( 'Location: home.php' );
-      exit(0);
-    }
+	//if($obj->active == 1)
+	//{
+		if($obj->password != "pass")
+		{
+			if(password_verify($_REQUEST['password'], $obj->password))
+			{
+				$_SESSION['userID'] = $obj->ID;
+				$smarty->assign("result","success");
+				header( 'Location: home.php' );
+				exit(0);
+			}
+		}
+		else if($obj->password == $_REQUEST['password'])
+		{
+			//@TODO redirect to change password page, once noone has the password "pass" remove this check
+			$_SESSION['userID'] = $obj->ID;
+			$smarty->assign("result","success");
+			header( 'Location: home.php' );
+			exit(0);
+		}
 		$smarty->assign("result","login_error");
-	}
+	/*}
 	else
 	{
 		$smarty->assign("result","activate");
-	}
+	}*/
 }
 
 $smarty->display("login.tpl");
