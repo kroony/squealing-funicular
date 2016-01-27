9cf7d1d977f60580c38c7b53ffc316c456e0177a
William Kerr <kroony@trout-slap.com>
2016-01-27 12:52:12 +1100

    tmp monster weapon fix

--- site/login.php
+++ site/login.php
@@ -14,8 +14,8 @@ if(!is_object($obj))
 }
 else
 {
-	if($obj->active == 1)
-	{
+	//if($obj->active == 1)
+	//{
 		if($obj->password != "pass")
 		{
 			if(password_verify($_REQUEST['password'], $obj->password))
@@ -35,11 +35,11 @@ else
 			exit(0);
 		}
 		$smarty->assign("result","login_error");
-	}
+	/*}
 	else
 	{
 		$smarty->assign("result","activate");
-	}
+	}*/
 }
 
 $smarty->display("login.tpl");
