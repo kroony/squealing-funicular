6cc487edc6547de0cbb5fa7a8134b701ce90cb5b
William Kerr <kroony@trout-slap.com>
2016-02-02 14:03:14 +1100

    NPC weapon

--- site/weaponBase.php
+++ site/weaponBase.php
@@ -8,12 +8,21 @@ $smarty->display("css/css.tpl");
 
 $weaponController = new weaponController();
 
+$db = DB::GetConn();
+
+print_r($db->query("UPDATE `weaponbase` SET `ID` = '5' WHERE `name` = 'Mace';"));
+print_r($db->query("UPDATE `weaponbase` SET `ID` = '6' WHERE `name` = 'Longbow';"));
+print_r($db->query("UPDATE `weaponbase` SET `ID` = '7' WHERE `name` = 'Rod';"));
+print_r($db->query("UPDATE `weaponbase` SET `ID` = '8' WHERE `name` = 'Quarterstaff';"));
+
+echo "RAN UPDATES";
+
 //menu
 $smarty->assign("currentpage","party");
 $smarty->display("menu.tpl");
 
 /*********  show all Party  ***********/
-$weaponBases = $weaponController->getAllWeaponBase();
+$weaponbases = $weaponController->getAllWeaponBase();
 $smarty->assign("weaponBases",$weaponBases);
 $smarty->display("weaponBase.tpl");
 /*********  end show all Party  ***********/
