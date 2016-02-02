7725c853e23df06c1a33e6f198779c096e26aee1
William Kerr <kroony@trout-slap.com>
2016-02-02 18:01:14 +1100

    commented out party page things that are not implemented

--- site/party.php
+++ site/party.php
@@ -6,16 +6,16 @@ include_once("party/Party.php");
 //html header
 $smarty->display("css/css.tpl");
 
-$partyController = new partyController();
+//$partyController = new partyController();
 
 //menu
 $smarty->assign("currentpage","party");
 $smarty->display("menu.tpl");
 
 /*********  show all Party  ***********/
-$userParties = $partyController->getAllForUser($currentUID);
-$smarty->assign("userParties",$userParties);
-$smarty->assign("totalParties",count($userParties));
+//$userParties = $partyController->getAllForUser($currentUID);
+//$smarty->assign("userParties",$userParties);
+//$smarty->assign("totalParties",count($userParties));
 $smarty->display("party.tpl");
 /*********  end show all Party  ***********/
 
