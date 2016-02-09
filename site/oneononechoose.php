803239d3862ad5a4cfc2b54c4a9a0c77418868b1
William Kerr <kroony@trout-slap.com>
2016-02-09 14:48:11 +1100

    Fight selection page Help

--- site/oneononechoose.php
+++ site/oneononechoose.php
@@ -3,6 +3,11 @@
 include("bootstrap.php");
 $smarty->display("css/css.tpl");
 
+//menu and Help
+$smarty->assign("help","This page displays all the Monsters and Player heroes in the world you can fight. 
+						A Monsters effectiveness is unknown before fighting them but if you succeed to knock them out, you will be rewarded will gold. 
+						You can only fight one level below your current, your current level, and up to two levels above your level when fighting other players heroes.");
+$smarty->assign("helpTitle","Fight Selection Page");
 $smarty->display("menu.tpl");
 
 include_once("hero/heroController.php");
