f4ac3df53146abd579dbd426879e114f7adf915b
William Kerr <kroony@trout-slap.com>
2016-04-08 12:35:06 +1000

    Run 00014_refactor

--- site/admin/migrations.php
+++ site/admin/migrations.php
@@ -4,6 +4,19 @@ chdir("../");
 
 include_once("bootstrap.php");
 
+$db = DB::GetConn();
+
+print_r($db->query("CREATE TABLE IF NOT EXISTS `Message` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ToID` int(11) NOT NULL,
+  `FromID` int(11) NOT NULL,
+  `Subject` text NOT NULL,
+  `Body` text NOT NULL,
+  `Sent` datetime NOT NULL,
+  `IsRead` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;"));
+
 //html header
 $smarty->display("css/css.tpl");
 
