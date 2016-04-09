860510783a0ff0a0bcbe1c3dd5c9fa4e281964fa
William Kerr <kroony@trout-slap.com>
2016-04-09 20:34:06 +1000

    admin view hero page

---
+++ site/admin/viewHero.php
@@ -0,0 +1,411 @@
+<?php
+
+include("bootstrap.php");
+include_once("hero/heroController.php");
+include_once("hero/weaponController.php");
+
+//load User
+include_once("user/user.php");
+$user = new User();
+$user = $user->load($currentUID);
+$smarty->assign("user",$user);
+
+$smarty->display("css/css.tpl");
+
+$smarty->assign("help","Hero and weapon names can be edited by clicking the edit button located next to the respective name.
+					    Equipped weapons can be changed if you have unused weapons in your inventory.
+					    When a hero gains a level they increase their Max HP and sometimes increase an attribute. This is shown with the green up arrow icon.");
+$smarty->assign("helpTitle","Hero Page Help");
+include_once("menu.php");
+
+$heroController = new heroController();
+$weaponController = new weaponController();
+
+$currentTime = new DateTime('now');
+$smarty->assign("currentTime", $currentTime);
+
+$hero = new Hero();
+$hero = $hero->loadHero($_REQUEST['ID']);
+
+//Class Tree Diagram
+$baseClass = $hero->HeroClass;
+$childClasses = array();
+if($baseClass->NextClass != null && $baseClass->NextClass != "")
+{
+	$childClassIDs = explode("|", $baseClass->NextClass);
+	foreach($childClassIDs as $childClassID)
+	{
+		array_push($childClasses, HeroClass::loadHeroClass($childClassID));
+	}
+}
+$smarty->assign("baseClass", $baseClass);
+$smarty->assign("childClasses", $childClasses);
+
+//Weapon
+$allWeapons = $weaponController->getAllForUser($currentUID);
+$unequipedWeapons = array();
+foreach($allWeapons as $weapon)
+{
+	if(!is_numeric($weapon->GetHeroIDFromWeapon()))
+	{
+		array_push($unequipedWeapons, $weapon);
+	}
+}
+if(count($unequipedWeapons) > 0)
+{
+	$smarty->assign("unequipedWeapons", $unequipedWeapons);
+}
+
+
+	if(isset($_REQUEST['action']))//check if we are doing anything
+	{	
+		if($_REQUEST['action'] == "editName")
+		{
+			if($_REQUEST['heroName'] != "" && $_REQUEST['heroName'] != null)
+			{
+				$oldName = $hero->Name;
+				$hero->Name = $_REQUEST['heroName'];
+				$hero->SaveHero();
+				
+				$smarty->assign("message", $oldName . " was renamed to " . $hero->Name);
+				$smarty->assign("hero",$hero);
+			}
+			else
+			{
+				$smarty->assign("error","Hero's name can not be blank");
+				$smarty->assign("hero",$hero);
+			}
+		}
+		else if($_REQUEST['action'] == "changeWeapon")
+		{
+			$weapon = Weapon::loadWeapon($_REQUEST['WeaponID']);
+			
+			if($weapon->UserID == $currentUID)
+			{
+				$hero->Weapon = $weapon;
+				$hero->SaveHero();
+				$smarty->assign("hero",$hero);
+				$smarty->assign("message", $hero->Name . " has equipped " . $weapon->Name);
+			}
+			else
+			{
+				$smarty->assign("error","That does not belong to you.");
+			}
+		}
+		else if($_REQUEST['action'] == "editWeaponName")
+		{
+			$weapon = Weapon::loadWeapon($_REQUEST['WeaponID']);
+			if($weapon->UserID == $currentUID)
+			{
+				$oldName = $weapon->Name;
+				$weapon->Name = $_REQUEST['weaponName'];
+				$weapon->save();
+				
+				$smarty->assign("message", $oldName . " was renamed to " . $weapon->Name);
+				$hero->Weapon = $weapon;
+				$smarty->assign("hero",$hero);
+			}
+			else
+			{
+				$smarty->assign("error","You can't rename what does not belong to you.");
+			}
+		}
+		else if($_REQUEST['action'] == "levelUp")
+		{
+			if($hero->canLevelUp())
+			{
+				if($hero->Status == "")
+				{
+					$hero->Status = "Level Up";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", $hero->Level))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun the process of Levelling up. It will take " . $hero->Level . " minutes to complete.");
+					$smarty->assign("hero",$hero);
+				}
+				else if($hero->Status == "Level Up")
+				{
+					$smarty->assign("error","The level up process has already begun.");
+					$smarty->assign("hero",$hero);
+				}
+				else
+				{
+					$smarty->assign("error","Hero is busy, cant level up at this time.");
+					$smarty->assign("hero",$hero);
+				}
+			}
+			else
+			{
+				if ($hero->CurrentXP < $hero->LevelUpXP)
+				{
+					$smarty->assign("error","Not Enough XP to level up.");
+					$smarty->assign("hero",$hero);
+				}
+				else
+				{
+					$smarty->assign("error","No class beyond this can be taken at this time.");
+					$smarty->assign("hero",$hero);
+				}
+			}
+		}
+		else if($_REQUEST['action'] == "FinishlevelUp")
+		{
+			if($hero->StatusETA == 'None' && $hero->Status == "Level Up")
+			{
+				$hero->Status = "";
+				
+				$preStr = $hero->Str;
+				$preDex = $hero->Dex;
+				$preCon = $hero->Con;
+				$preIntel = $hero->Intel;
+				$preWis = $hero->Wis;
+				$preCha = $hero->Cha;
+				$preLevel = $hero->Level;
+				$preClass = $hero->HeroClass->Name;
+				$preHP = $hero->MaxHP;
+				$preXP = $hero->CurrentXP;
+				
+				$returnString = $hero->LevelUP();
+				
+				if($returnString == "Not Enough XP to level up")
+				{
+					$hero->Status = "";
+					$hero->SaveHero();
+					$smarty->assign("error","Not Enough XP to level up.");
+					$smarty->assign("hero",$hero);
+				}
+				else
+				{
+					if($preStr < $hero->Str){$smarty->assign("StrIncrease", true);}
+					if($preDex < $hero->Dex){$smarty->assign("DexIncrease", true);}
+					if($preCon < $hero->Con){$smarty->assign("ConIncrease", true);}
+					if($preIntel < $hero->Intel){$smarty->assign("IntelIncrease", true);}
+					if($preWis < $hero->Wis){$smarty->assign("WisIncrease", true);}
+					if($preCha < $hero->Cha){$smarty->assign("ChaIncrease", true);}
+					
+					if($preLevel < $hero->Level){$smarty->assign("LevelIncrease", true);}
+					if($preClass != $hero->HeroClass->Name){$smarty->assign("ClassChange", $preClass);}
+					if($preHP < $hero->MaxHP){$smarty->assign("HPIncrease", $hero->MaxHP - $preHP);}
+					if($preXP == 0){$smarty->assign("OldXP", $preXP);}
+					
+					$smarty->assign("message", $returnString);
+					// if we levelled up then we can save hero
+					$hero->SaveHero();
+					
+					$smarty->assign("hero",$hero);
+				}
+			}
+			else
+			{
+				$smarty->assign("error","Have not finished levelling up");
+				$smarty->assign("hero",$hero);
+			}
+		}
+		else if($_REQUEST['action'] == "Train")
+		{
+			//load User for gold spending
+			include_once("user/user.php");
+			$user = new User();
+			$user = $user->load($currentUID);
+
+			if($_REQUEST['increase'] == "Str")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Str)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Str);
+					$user->Save();
+					
+					$hero->Status = "Train Str";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", ($hero->Str + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Strength. It will take " . (($hero->Str + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Strength");
+				}
+			}
+			else if($_REQUEST['increase'] == "Dex")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Dex)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Dex);
+					$user->Save();
+					
+					$hero->Status = "Train Dex";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", ($hero->Dex + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Dexterity. It will take " . (($hero->Dex + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Dexterity");
+				}
+			}
+			else if($_REQUEST['increase'] == "Con")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Con)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Con);
+					$user->Save();
+					
+					$hero->Status = "Train Con";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", ($hero->Con + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Constitution. It will take " . (($hero->Con + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Constitution");
+				}
+			}
+			else if($_REQUEST['increase'] == "Intel")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Intel)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Intel);
+					$user->Save();
+					
+					$hero->Status = "Train Intel";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", ($hero->Intel + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Intelligence. It will take " . (($hero->Intel + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Intelligence");
+				}
+			}
+			else if($_REQUEST['increase'] == "Wis")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Wis)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Wis);
+					$user->Save();
+					
+					$hero->Status = "Train Wis";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes",($hero->Wis + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Wisdom. It will take " . (($hero->Wis + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Wisdom");
+				}
+			}
+			else if($_REQUEST['increase'] == "Cha")
+			{
+				if($user->canAfford($hero->calculateAttributeUpgradeCost($hero->Cha)))
+				{
+					$user->gold -= $hero->calculateAttributeUpgradeCost($hero->Cha);
+					$user->Save();
+					
+					$hero->Status = "Train Cha";
+					$hero->StatusTime = new DateTime(date("Y-m-d H:i:s", strtotime(sprintf("+%d minutes", ($hero->Cha + 1) * 10))));
+					$hero->SaveHero();
+					$hero = $hero->loadHero($_REQUEST['ID']);//load to get get the time 
+					
+					$smarty->assign("message", $hero->Name . " has begun training their Charisma. It will take " . (($hero->Cha + 1) * 10) . " minutes to complete.");
+				}
+				else
+				{
+					$smarty->assign("error","Can not afford to increase Charisma");
+				}
+			}
+		}
+		else if($_REQUEST['action'] == "FinishTrain")
+		{
+			if($hero->StatusETA == 'None')
+			{
+				if($_REQUEST['increase'] == "Str" && $hero->Status == "Train Str")
+				{
+					$hero->Str++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Strength training.");
+					$smarty->assign("StrIncrease", true);
+				}
+				else if($_REQUEST['increase'] == "Dex" && $hero->Status == "Train Dex")
+				{
+					$hero->Dex++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Dexterity training.");
+					$smarty->assign("DexIncrease", true);
+				}
+				else if($_REQUEST['increase'] == "Con" && $hero->Status == "Train Con")
+				{
+					$hero->Con++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Constitution training.");
+					$smarty->assign("ConIncrease", true);
+				}
+				else if($_REQUEST['increase'] == "Intel" && $hero->Status == "Train Intel")
+				{
+					$hero->Intel++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Intelligence training.");
+					$smarty->assign("IntelIncrease", true);
+				}
+				else if($_REQUEST['increase'] == "Wis" && $hero->Status == "Train Wis")
+				{
+					$hero->Wis++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Wisdom training.");
+					$smarty->assign("WisIncrease", true);
+				}
+				else if($_REQUEST['increase'] == "Cha" && $hero->Status == "Train Cha")
+				{
+					$hero->Cha++;
+					$hero->Status = "";
+					$hero->SaveHero();
+					
+					//outputs
+					$smarty->assign("message", $hero->Name . " has finished their Charisma training.");
+					$smarty->assign("ChaIncrease", true);
+				}
+				else
+				{
+					$smarty->assign("error","A problem occurred attempting to finish training");
+				}
+			}
+			else
+			{
+				$smarty->assign("error","Have not finished training time");
+			}
+		}
+	}
+	else// we are just viewing hero
+	{
+		$smarty->assign("hero",$hero);
+	}
+	$smarty->assign("hero",$hero);
+
+
+$smarty->display("viewHero.tpl");
+?>
+
---
+++ site/admin/viewHero.tpl
@@ -0,0 +1,207 @@
+<div class="container-fluid">
+<div id="heroName"><h3>{$hero->Name} <span class="glyphicon glyphicon-edit" data-toggle="tooltip" title="Edit Name" onclick='document.getElementById("heroName").style.display = "none"; document.getElementById("heroEditName").style.display = "block";'></span></h3></div>
+
+{if isset($message)}
+	<div class="alert alert-info">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		{$message}
+	</div>
+{/if}
+{if isset($error)}
+	<div class="alert alert-danger">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		{$error}
+	</div>
+{/if}
+
+<form id="heroEditName" action="viewHero.php" style="display: none;">
+<input type="hidden" name="action" value="editName">
+<input type="hidden" name="ID" value="{$hero->ID}">
+<input type="text" name="heroName" value="{$hero->Name}">
+<input type="submit" value="Submit">
+</form>
+<br />
+{include file='portraits/kobold.html'}
+<br />
+Level: {$hero->Level}{if isset($LevelIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span>{/if}<br />
+Race: {$hero->Race->Name} - {$hero->Race->Description}<br />
+Class: {if isset($ClassChange)} After mastering all that a {$ClassChange} can. {$hero->Name} has advanced to <br />{/if}
+<strong>{$hero->HeroClass->Name}</strong><br />{$hero->HeroClass->Description}<br /><i>"{$hero->HeroClass->Quote}"</i><br />
+<div class="progress">
+  <div class="progress-bar {if $displayHero->CurrentHP == $displayHero->MaxHP} progress-bar-success {elseif $displayHero->CurrentHP < $displayHero->Con} progress-bar-danger {elseif $displayHero->CurrentHP < $displayHero->MaxHP} progress-bar-warning {/if}" 
+  role="progressbar" aria-valuenow="{$hero->CurrentHP}" aria-valuemin="0" aria-valuemax="{$hero->MaxHP}" style="width:{$hero->CurrentHP/$hero->MaxHP*100}%">
+	<span>
+		{$hero->CurrentHP}HP/{$hero->MaxHP}HP{if isset($HPIncrease)} <strong>+{$HPIncrease}</strong>{/if}
+		{if $hero->isAlive() == false} <a href='delete.php?ID={$hero->ID}'>Remove</a>{elseif $hero->CurrentHP <= 0} <a href='revive.php?ID={$hero->ID}'>Revive</a>{/if}
+	</span>
+  </div>
+</div><br />
+
+<div class="progress">
+  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="{$hero->CurrentXP}"
+  aria-valuemin="0" aria-valuemax="{$hero->LevelUpXP}" id="XPBar" style="width:{if isset($OldXP)}{$OldXP/$hero->LevelUpXP*100}{else}{$hero->CurrentXP/$hero->LevelUpXP*100}{/if}%">
+    <span>{number_format($hero->CurrentXP)}XP/{number_format($hero->LevelUpXP)}XP{if $hero->CurrentXP >= $hero->LevelUpXP && $hero->Status == ""} <a href="viewHero.php?action=levelUp&ID={$hero->ID}">Try Level up</a>{/if}</span>
+  </div>
+</div>
+{if isset($OldXP)}
+	<script type="text/javascript">
+		UpdateBar("XPBar", 0, {$hero->CurrentXP});
+	</script>
+{/if}
+{if $hero->Status == "Level Up" && $hero->StatusETA != 'None'}
+	Currently levelling up, <span id="LevelStatusCountdown"></span> remaining.
+	<script type="text/javascript">
+		countdown( "LevelStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+	</script>
+{elseif $hero->Status == "Level Up" && $hero->StatusETA == 'None'}
+	<a href="viewHero.php?action=FinishlevelUp&ID={$hero->ID}">Complete Level Up!</a>
+{/if}
+<br />
+Strength: {$hero->Str}
+	{if isset($StrIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Strength has increased!{/if} 
+	{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Str))}<a href="viewHero.php?action=Train&increase=Str&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Str))}gp</a>{/if}
+	{elseif $hero->Status == "Train Str" && $hero->StatusETA != 'None'}
+		Currently training, <span id="StrStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "StrStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Str" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Str&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+Dexterity: {$hero->Dex}
+	{if isset($DexIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Dexterity has increased!{/if}
+	{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Dex))}<a href="viewHero.php?action=Train&increase=Dex&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Dex))}gp</a>{/if}
+	{elseif $hero->Status == "Train Dex" && $hero->StatusETA != 'None'}
+		Currently training, <span id="DexStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "DexStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Dex" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Dex&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+Constitution: {$hero->Con}
+	{if isset($ConIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Constitution has increased!{/if}
+	{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Con))}<a href="viewHero.php?action=Train&increase=Con&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Con))}gp</a>{/if}
+	{elseif $hero->Status == "Train Con" && $hero->StatusETA != 'None'}
+		Currently training, <span id="ConStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "ConStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Con" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Con&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+Intelligence: {$hero->Intel}
+	{if isset($IntelIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Intelligence has increased!{/if}
+	{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Intel))}<a href="viewHero.php?action=Train&increase=Intel&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Intel))}gp</a>{/if}
+	{elseif $hero->Status == "Train Intel" && $hero->StatusETA != 'None'}
+		Currently training, <span id="IntelStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "IntelStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Intel" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Intel&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+Wisdom: {$hero->Wis}
+	{if isset($WisIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Wisdom has increased!{/if}
+		{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Wis))}<a href="viewHero.php?action=Train&increase=Wis&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Wis))}gp</a>{/if}
+	{elseif $hero->Status == "Train Wis" && $hero->StatusETA != 'None'}
+		Currently training, <span id="WisStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "WisStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Wis" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Wis&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+Charisma: {$hero->Cha}
+	{if isset($ChaIncrease)} <span class="glyphicon glyphicon-arrow-up" style="color: limegreen;"> +1</span> Charisma has increased!{/if}
+		{if $hero->Status == ""}
+		{if $user->canAfford($hero->calculateAttributeUpgradeCost($hero->Cha))}<a href="viewHero.php?action=Train&increase=Cha&ID={$hero->ID}">Train for {number_format($hero->calculateAttributeUpgradeCost($hero->Cha))}gp</a>{/if}
+	{elseif $hero->Status == "Train Cha" && $hero->StatusETA != 'None'}
+		Currently training, <span id="ChaStatusCountdown"></span> remaining.
+		<script type="text/javascript">
+			countdown( "ChaStatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+		</script>
+	{elseif $hero->Status == "Train Cha" && $hero->StatusETA == 'None'}
+		<a href="viewHero.php?action=FinishTrain&increase=Cha&ID={$hero->ID}">Complete Training!</a>
+	{/if}
+<br />
+<br /><strong>Statistics</strong>
+<br />Age: {$hero->Age} years
+<br />Kills: {$hero->Kills}
+<br />
+<br /><strong>Debug</strong>
+<br />Status: {$hero->Status}
+<br />StatusTime: {$hero->StatusTime->format('Y-m-d H:i:s')}
+<br />NOW: {$currentTime->format("Y-m-d H:m:s")}
+{if $hero->StatusETA != 'None'}
+	<br />Difference: <span id="StatusCountdown"></span>
+	<script type="text/javascript">
+		countdown( "StatusCountdown", {$hero->getStatusCountdownJSArgs()} );
+	</script>
+{/if}
+<br />
+{include file='classtreeSmall.tpl'}
+<br />
+<div class="row">
+	<div class="col-sm-4" >
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<div id="weaponNameDiv"><b>{$hero->Weapon->Name}</b> <span class="glyphicon glyphicon-edit" data-toggle="tooltip" title="Edit Name"  onclick='document.getElementById("weaponNameDiv").style.display = "none"; document.getElementById("weaponEditNameForm").style.display = "block";' ></span></div>
+				<form id="weaponEditNameForm" action="viewHero.php" style="display: none;">
+					<input type="hidden" name="action" value="editWeaponName">
+					<input type="hidden" name="ID" value="{$hero->ID}">
+					<input type="hidden" name="WeaponID" value="{$hero->Weapon->ID}">
+					<input type="text" name="weaponName" value="{$hero->Weapon->Name}">
+					<input type="submit" value="Submit">
+				</form>
+			</div>
+			<div class="panel-body">
+				<!--<i>@TODO: image</i><br />-->
+				Damage: {$hero->Weapon->DamageQuantity}d{$hero->Weapon->DamageDie}{if $hero->Weapon->DamageOffset < 0}{$hero->Weapon->DamageOffset}{elseif $hero->Weapon->DamageOffset > 0}+{$hero->Weapon->DamageOffset}{/if}<br />
+				Uses: {$hero->Weapon->DamageAttribute} (+{$hero->calculateAttributeBonus($hero->getAttributeByName($hero->Weapon->DamageAttribute))})<br />
+				Critical Strike Chance: {$hero->Weapon->CritChance}%<br />
+				<br />
+				{if isset($unequipedWeapons)}
+				Change Weapon:<br />
+				<form id="changeWeapon" action="viewHero.php">
+					<input type="hidden" name="action" value="changeWeapon">
+					<input type="hidden" name="ID" value="{$hero->ID}">
+					<select name="WeaponID">
+						<option>Select Weapon</option>
+						{foreach from=$unequipedWeapons item=weapon}
+						<option value="{$weapon->ID}">{$weapon->Name} {$weapon->DamageQuantity}d{$weapon->DamageDie}{if $weapon->DamageOffset < 0}{$weapon->DamageOffset}{elseif $weapon->DamageOffset > 0}+{$weapon->DamageOffset}{/if}</option>
+						{/foreach}
+					</select>
+					<input type="submit" value="Submit">
+				</form>
+				{/if}
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel panel-default">
+			<div class="panel-heading">Armor</div>
+			<div class="panel-body">
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="panel panel-default">
+			<div class="panel-heading">Item</div>
+			<div class="panel-body">
+			</div>
+		</div>
+	</div>
+</div>
+
+</div>
