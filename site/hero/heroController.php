a1d477f6aba54d9f70c32f13d9794b8e96f1605c
William Kerr <kroony@trout-slap.com>
2018-10-25 18:03:57 +1100

    only attack ppl in town

--- site/hero/heroController.php
+++ site/hero/heroController.php
@@ -159,6 +159,7 @@ class heroController
 		$getQuery = "	(
                     SELECT Hero.* FROM `Hero` 
                     WHERE `OwnerID` <> $id AND
+                    `Location` = 'town' AND
                     `Level` BETWEEN $low_level AND $high_level AND
                     (
                       (`Status` = '' AND `CurrentHP` = `MaxHP`) OR 
