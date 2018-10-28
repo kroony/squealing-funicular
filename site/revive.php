92978f26168d16eb882c3aecb4ce8c849c3804d9
William Kerr <kroony@trout-slap.com>
2018-10-29 00:20:54 +1100

    pay for revive

--- site/revive.php
+++ site/revive.php
@@ -1,13 +1,28 @@
 <?php
-
 include("bootstrap.php");
 
 /*********Add XP*********/
 include_once("hero/hero.php");
+include_once("user/user.php");
 
 $hero = new Hero();
 $hero = $hero->loadHero($_REQUEST['ID']);
-$hero->revive();
-$hero->SaveHero();
 
-header("Location: guild.php");
+$user = new User();
+$user = $user->load($hero->OwnerID);
+
+$reviveCost = $hero->calculateReviveCost();
+if($user->canAfford($reviveCost))
+{
+  $user->debit($reviveCost);
+  $hero = $hero->revive();
+  $hero->SaveHero();
+  
+  header("Location: healer.php?message=" . $hero->Name . " has been revived.");
+  exit;
+}
+else
+{
+  header("Location: healer.php?message=Can not afford to revive " . $hero->Name);
+  exit;
+}			
